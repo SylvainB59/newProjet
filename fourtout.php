25a65af47f5eda732217f184f6269b09e84ca2b0
59013-76-07 <59013-76-07@exchange.ad.afpanet>
2021-02-01 11:03:09 +0100

    ajout de fichiers

---
+++ fourtout.php
@@ -0,0 +1,14 @@
+plik.root.gg <!-- envoyer des fichiers -->
+
+**Git/gitHub**
+- dans le dossier voulu: clic droit -> git bash here
+    - sur gtihub, créer un nouveau repositori
+    - dans le bash -> git init
+
+- Ajout de fichiers a la "stage area" -> git add .
+
+- vérification des fichiers ajoutés -> git status
+
+- ajout du serveur distant -> git remote add origin git@github.com:SylvainB59/newProjet.git
+
+- attribuer un nom a la version -> git commit -m "ajout des fichiers"
